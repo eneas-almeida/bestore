960443505a29c835514066a3a6a45c6612be68cd
Venzel (nt) <venzelsoft@gmail.com>
2021-07-23 21:01:50 -0300

    UserRepository e User: modificados

--- src/main/java/br/com/venzel/store/modules/user/entities/User.java
+++ src/main/java/br/com/venzel/store/modules/user/entities/User.java
@@ -19,4 +19,6 @@ public class User {
     private Long id;
 
     private String name;
+
+    private String email;
 }
--- src/main/java/br/com/venzel/store/modules/user/repositories/UserRepository.java
+++ src/main/java/br/com/venzel/store/modules/user/repositories/UserRepository.java
@@ -1,7 +1,29 @@
 package br.com.venzel.store.modules.user.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.venzel.store.modules.user.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findOneById(Long id);
+
+    Optional<User> findOneByEmail(String email);
+
+    Optional<User> findOneByName(String name);
+    
+    List<User> findAllByNameContaining(String name);
+
+    Optional<User> findFirstByNameContaining(String name);
+
+    List<User> findTop5ByNameContaining(String name);
+
+    Boolean existsByName(String name);
+
+    Boolean existsByEmail(String email);
+
+    Integer countByName(String name);
+}
