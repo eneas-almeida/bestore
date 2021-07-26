cb72b4979a2dbf28d22902a012d4727cedac8690
Venzel home <venzelseo@gmail.com>
2021-07-25 23:38:45 -0300

    Fix: pequenas correcoes

--- src/main/java/br/com/venzel/store/modules/user/repositories/UserRepository.java
+++ src/main/java/br/com/venzel/store/modules/user/repositories/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.venzel.store.modules.user.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findOneById(Long id);
