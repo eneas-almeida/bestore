070b87079a5c789f0135165816ebf075d50c0eb9
Venzel home <venzelseo@gmail.com>
2021-07-30 17:00:32 -0300

    ShowPayment: criado o use case

---
+++ src/main/java/br/com/venzel/store/modules/payment/use_cases/show_payment/ShowPaymentService.java
@@ -0,0 +1,37 @@
+package br.com.venzel.store.modules.payment.use_cases.show_payment;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.venzel.store.modules.payment.dtos.PaymentDTO;
+import br.com.venzel.store.modules.payment.entities.Payment;
+import br.com.venzel.store.modules.payment.exceptions.PaymentNotFoundException;
+import br.com.venzel.store.modules.payment.mapper.PaymentMapper;
+import br.com.venzel.store.modules.payment.repositories.PaymentRepository;
+import br.com.venzel.store.modules.payment.utils.PaymentMessageUtils;
+
+@Service
+public class ShowPaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private PaymentMapper paymentMapper;
+
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    public PaymentDTO execute(Long id) {
+        
+        Optional<Payment> optionalEntity = paymentRepository.findById(id);
+
+        if (!optionalEntity.isPresent()) {
+            throw new PaymentNotFoundException(PaymentMessageUtils.PAYMENT_NOT_FOUND);
+        }
+
+        return paymentMapper.toDTO(optionalEntity.get());
+    }  
+}
