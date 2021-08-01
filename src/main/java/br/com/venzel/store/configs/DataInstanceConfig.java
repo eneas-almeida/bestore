0e140ad06aecf7e7621987497e2f9f8f627f6838
Venzel home <venzelseo@gmail.com>
2021-08-01 00:38:46 -0300

    Address: modulo criado

--- src/main/java/br/com/venzel/store/configs/DataInstanceConfig.java
+++ src/main/java/br/com/venzel/store/configs/DataInstanceConfig.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.venzel.store.modules.address.entities.City;
+import br.com.venzel.store.modules.address.entities.State;
+import br.com.venzel.store.modules.address.repositories.CityRepository;
+import br.com.venzel.store.modules.address.repositories.StateRepository;
 import br.com.venzel.store.modules.order.entities.Order;
 import br.com.venzel.store.modules.order.repositories.OrderRepository;
 import br.com.venzel.store.modules.payment.entities.Payment;
@@ -41,48 +45,75 @@ public class DataInstanceConfig implements CommandLineRunner {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    @Autowired
+    private StateRepository stateRepository;
+    
+    @Autowired
+    private CityRepository cityRepository;
+
+
     @Override
     public void run(String... args) throws Exception {
         
         /* */
 
-        Category c1 = new Category("cereais");
-        Category c2 = new Category("padaria");
-        Category c3 = new Category("mercearia");
+        Category ca1 = new Category("cereais");
+        Category ca2 = new Category("padaria");
+        Category ca3 = new Category("mercearia");
 
-        Product p1 = new Product("feijao", 10.21);
-        Product p2 = new Product("arroz", 7.44);
-        Product p3 = new Product("cuzcuz", 3.76);
+        Product pd1 = new Product("feijao", 10.21);
+        Product pd2 = new Product("arroz", 7.44);
+        Product pd3 = new Product("cuzcuz", 3.76);
 
-        User u1 = new User("Tiago Rizzo", "tiago@gmail.com", hashProvider.generateHash("tiago"));
-        User u2 = new User("Alex Moura", "alex@gmail.com", hashProvider.generateHash("alex"));
-        User u3 = new User("Liz Venzel", "liz@gmail.com", hashProvider.generateHash("liz"));
+        User us1 = new User("Tiago Rizzo", "tiago@gmail.com", hashProvider.generateHash("tiago"));
+        User us2 = new User("Alex Moura", "alex@gmail.com", hashProvider.generateHash("alex"));
+        User us3 = new User("Liz Venzel", "liz@gmail.com", hashProvider.generateHash("liz"));
 
-        Order o1 = new Order();
-        Order o2 = new Order();
-        Order o3 = new Order();
+        Order od1 = new Order();
+        Order od2 = new Order();
+        Order od3 = new Order();
 
-        Payment py1 = new Payment("SEPARACAO");
-        Payment py2 = new Payment("SEPARACAO");
-        Payment py3 = new Payment("SEPARACAO");
+        Payment pm1 = new Payment("SEPARACAO");
+        Payment pm2 = new Payment("SEPARACAO");
+        Payment pm3 = new Payment("SEPARACAO");
+
+        State st1 = new State("SP");
+        State st2 = new State("RJ");
+        State st3 = new State("PE");
+        State st4 = new State("PB");
+
+        City ct1 = new City("Bauru", st1);
+        City ct2 = new City("Campinas", st1);
+        City ct3 = new City("Macae", st2);
+        City ct4 = new City("Recife", st3);
+        City ct5 = new City("Campina Grande", st4);
 
         /* */
 
-        c1.getProducts().addAll(Arrays.asList(p1, p2, p3));
-        c2.getProducts().addAll(Arrays.asList(p2));
+        ca1.getProducts().addAll(Arrays.asList(pd1, pd2, pd3));
+        ca2.getProducts().addAll(Arrays.asList(pd2));
 
         /* */
 
-        p1.getCategories().addAll(Arrays.asList(c1));
-        p2.getCategories().addAll(Arrays.asList(c1, c2));
-        p3.getCategories().addAll(Arrays.asList(c1));
+        pd1.getCategories().addAll(Arrays.asList(ca1));
+        pd2.getCategories().addAll(Arrays.asList(ca1, ca2));
+        pd3.getCategories().addAll(Arrays.asList(ca1));
 
         /* */
 
-        categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
-        productRepository.saveAll(Arrays.asList(p1, p2, p3));
-        userRepository.saveAll(Arrays.asList(u1, u2, u3));
-        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-        paymentRepository.saveAll(Arrays.asList(py1, py2, py3));
+        st1.getCities().addAll(Arrays.asList(ct1, ct2));
+        st2.getCities().addAll(Arrays.asList(ct3));
+        st3.getCities().addAll(Arrays.asList(ct4));
+        st4.getCities().addAll(Arrays.asList(ct5));
+
+        /* */
+
+        categoryRepository.saveAll(Arrays.asList(ca1, ca2, ca3));
+        productRepository.saveAll(Arrays.asList(pd1, pd2, pd3));
+        userRepository.saveAll(Arrays.asList(us1, us2, us3));
+        orderRepository.saveAll(Arrays.asList(od1, od2, od3));
+        paymentRepository.saveAll(Arrays.asList(pm1, pm2, pm3));
+        stateRepository.saveAll(Arrays.asList(st1, st2, st3, st4));
+        cityRepository.saveAll(Arrays.asList(ct1, ct2, ct3, ct4, ct5));
     }
 }
